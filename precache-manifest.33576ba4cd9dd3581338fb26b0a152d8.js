ed5d54290d4039d670596ddccc11181327d4e485
Ahmed Fasih <wuzzyview@gmail.com>
2020-08-18 11:34:00 -0400

    build

---
+++ precache-manifest.33576ba4cd9dd3581338fb26b0a152d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f94b6770f25d1d6229b4f824fcdda2",
+    "url": "/recoil-issue-522/index.html"
+  },
+  {
+    "revision": "9d3038d2001295d9c332",
+    "url": "/recoil-issue-522/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b2023e2303a8a563d3e",
+    "url": "/recoil-issue-522/static/js/2.44a9acc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recoil-issue-522/static/js/2.44a9acc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d3038d2001295d9c332",
+    "url": "/recoil-issue-522/static/js/main.b84166cb.chunk.js"
+  },
+  {
+    "revision": "82f5927a54bb884a3850",
+    "url": "/recoil-issue-522/static/js/runtime-main.44995f55.js"
+  }
+]);
