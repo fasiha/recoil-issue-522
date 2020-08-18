623c65b51cf43ab487c9eeba7447e8e8a7a54efc
Ahmed Fasih <wuzzyview@gmail.com>
2020-08-18 11:27:20 -0400

    minimal example for facebookexperimental/Recoil#22

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import {createElement} from 'react';
+import {atom, selectorFamily, useRecoilValue, useSetRecoilState} from 'recoil';
+
+const ce = createElement;
+
+/*
+RECOIL
+*/
+const names = ['a', 'b', 'c'];  // fixed names for each row for our app
+const appAtom = atom({
+  key: 'app',
+  default: {a: ['init'], b: ['hello'], c: ['world']}
+});  // same keys as `names` above
+const appSubkeySelector = selectorFamily(
+    {key: 'appSubkey', get: key => ({get}) => get(appAtom)[key]});
+
+/*
+REACT
+*/
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ce('div', null, ...names.map(name => ce(Row, {name})), ce(AddButton));
+}
+
+function Row({name}) {
+  const elements = useRecoilValue(appSubkeySelector(name));
+  console.log('RERENDERING ' + name);
+  return ce('ol', null, ...elements.map(element => ce('li', null, element)));
+}
+
+function AddButton() {
+  const setState = useSetRecoilState(appAtom);
+  return ce(
+      'button', {
+        onClick: () => {
+          const randomIdx = Math.floor(Math.random() * names.length);
+          const randomName = names[randomIdx];
+          const newElement = (new Date()).toISOString()
+          setState(
+              old =>
+                  ({...old, [randomName]: old[randomName].concat(newElement)}))
+        }
+      },
+      'Add')
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {RecoilRoot} from 'recoil';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+    <React.StrictMode><RecoilRoot><App /></RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
